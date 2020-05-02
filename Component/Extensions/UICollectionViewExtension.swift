9a57530e3d7dfbe06903f8b7265dc1a062e6d4f0
kimiaki704 <kimiaki704@gmail.com>
2020-05-03 04:30:43 +0900

    🐤 Update UICollectionVIewExtension.swift
    fix small

--- Component/Extensions/UICollectionViewExtension.swift
+++ Component/Extensions/UICollectionViewExtension.swift
@@ -1,5 +1,5 @@
 //
-//  UICollectionVIewExtension.swift
+//  UICollectionViewExtension.swift
 //  Component
 //
 //  Created by 鈴木 公章 on 2020/05/03.
@@ -8,11 +8,11 @@
 import UIKit
 
 extension UICollectionView {
-    func register<T: UICollectionViewCell>(_: T.Type) where T: NibLoadableView, T: ReusableView {
+    public func register<T: UICollectionViewCell>(_: T.Type) where T: NibLoadableView, T: ReusableView {
         register(T.nib, forCellWithReuseIdentifier: T.reuseIdentifier)
     }
 
-    func dequeueReusableCell<T: UICollectionViewCell>(forIndexPath indexPath: IndexPath) -> T where T: ReusableView {
+    public func dequeueReusableCell<T: UICollectionViewCell>(forIndexPath indexPath: IndexPath) -> T where T: ReusableView {
         guard let cell = dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier, for: indexPath) as? T else { fatalError("Cannot create new cell") }
         return cell
     }
