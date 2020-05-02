c852a6c20a7e717ec780ecd9e00eed01c630b649
Suzuki Kimiaki <kimiaki@suzukinoMacBook-Pro.local>
2020-05-02 17:59:10 +0900

    🍹 Add APIClient.swift

---
+++ Model/API/Base/APIClient.swift
@@ -0,0 +1,33 @@
+//
+//  APIClient.swift
+//  Model
+//
+//  Created by Suzuki Kimiaki on 2020/05/02.
+//
+
+import Alamofire
+
+public class APIClient {
+    func request(endpoint: Endpoint,
+                 parameters: [String: Any]?,
+                 headers: HTTPHeaders?,
+                 completion: @escaping (Result<APIResponce, Error>) -> Void) {
+        AF.request(endpoint.url,
+                   method: endpoint.method,
+                   parameters: parameters,
+                   headers: headers)
+            .validate(statusCode: 200 ..< 400)
+            .validate(contentType: ["application/json"])
+            .response { response in
+                switch response.result {
+                case .success:
+                    let res = APIResponce(urlReqest: response.request,
+                                          headers: response.response,
+                                          data: response.data)
+                    completion(.success(res))
+                case .failure:
+                    #warning("TODO: Error処理")
+                }
+            }
+    }
+}
