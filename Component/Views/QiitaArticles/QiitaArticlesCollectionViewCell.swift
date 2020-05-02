3ce204987cd2d8711daf7d7071e7f6021f61428f
kimiaki704 <kimiaki704@gmail.com>
2020-05-03 04:32:31 +0900

    🐤 Update QiitaArticlesCollectionViewCell
    add logic

--- Component/Views/QiitaArticles/QiitaArticlesCollectionViewCell.swift
+++ Component/Views/QiitaArticles/QiitaArticlesCollectionViewCell.swift
@@ -5,6 +5,19 @@
 //  Created by 鈴木 公章 on 2020/05/03.
 //
 
+import Model
 import UIKit
 
-final class QiitaArticlesCollectionViewCell: UICollectionViewCell, NibLoadableView {}
+public class QiitaArticlesCollectionViewCell: UICollectionViewCell, NibLoadableView, ReusableView {
+    @IBOutlet private var userImageView: UIImageView!
+    @IBOutlet private var articleTitleLabel: UILabel!
+    @IBOutlet private var userNameLabel: UILabel!
+    @IBOutlet private var lgtmCountLabel: UILabel!
+
+    public func setup(_ article: QiitaArticle) {
+//        userImageView.image = article.userImgURL
+        articleTitleLabel.text = article.title
+        userNameLabel.text = "by \(article.userName)"
+        lgtmCountLabel.text = "LGTM \(article.likeCount)"
+    }
+}
