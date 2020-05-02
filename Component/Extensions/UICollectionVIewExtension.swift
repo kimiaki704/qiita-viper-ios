7c8b5fa21035ae881f60a4f553156e7769fc4082
kimiaki704 <kimiaki704@gmail.com>
2020-05-03 03:49:51 +0900

    🐤 Update UICollectionVIewExtension.swift
    add dequeueReusableCell

--- Component/Extensions/UICollectionVIewExtension.swift
+++ Component/Extensions/UICollectionVIewExtension.swift
@@ -11,4 +11,9 @@ extension UICollectionView {
     func register<T: UICollectionViewCell>(_: T.Type) where T: NibLoadableView, T: ReusableView {
         register(T.nib, forCellWithReuseIdentifier: T.reuseIdentifier)
     }
+
+    func dequeueReusableCell<T: UICollectionViewCell>(forIndexPath indexPath: IndexPath) -> T where T: ReusableView {
+        guard let cell = dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier, for: indexPath) as? T else { fatalError("Cannot create new cell") }
+        return cell
+    }
 }
