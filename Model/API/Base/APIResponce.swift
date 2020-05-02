5179962e46eb44609b909194be8bda835a2c2465
Suzuki Kimiaki <kimiaki@suzukinoMacBook-Pro.local>
2020-05-02 17:55:40 +0900

    🍹 Add APIResponce.swift

---
+++ Model/API/Base/APIResponce.swift
@@ -0,0 +1,12 @@
+//
+//  APIResponce.swift
+//  Model
+//
+//  Created by Suzuki Kimiaki on 2020/05/02.
+//
+
+public struct APIResponce {
+    public let urlReqest: URLRequest?
+    public let headers: HTTPURLResponse?
+    public let data: Data?
+}
