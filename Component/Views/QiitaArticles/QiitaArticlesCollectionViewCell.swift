10ee61d36c070961695b602e4c50774ae5ebb54d
kimiaki704 <kimiaki704@gmail.com>
2020-05-03 03:29:50 +0900

    🍹 Add QiitaArticlesCollectionViewCell

---
+++ Component/Views/QiitaArticles/QiitaArticlesCollectionViewCell.swift
@@ -0,0 +1,10 @@
+//
+//  QiitaArticlesCollectionViewCell.swift
+//  Component
+//
+//  Created by 鈴木 公章 on 2020/05/03.
+//
+
+import UIKit
+
+final class QiitaArticlesCollectionViewCell: UICollectionViewCell, NibLoadableView {}
