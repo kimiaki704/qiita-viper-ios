ce07c56dbf03189ede43414fb3ce1c7e07942702
Suzuki Kimiaki <kimiaki@suzukinoMacBook-Pro.local>
2020-05-02 15:39:45 +0900

    🐤 Update ViewProtocols.swift
    add Instantiatable protocol
    remove ReusableView protocol
    add nibName and nibBundle property to NibLoadableView protocol

--- Component/Protocols/ViewProtocols.swift
+++ Component/Protocols/ViewProtocols.swift
@@ -5,5 +5,12 @@
 //  Created by Suzuki Kimiaki on 2020/05/02.
 //
 
-protocol ReusableView: class {}
-protocol NibLoadableView: class {}
+protocol Instantiatable: class {
+    static var storyboardName: String { get }
+    static var bundle: Bundle? { get }
+}
+
+protocol NibLoadableView: class {
+    static var nibName: String { get }
+    static var nibBundle: Bundle? { get }
+}
