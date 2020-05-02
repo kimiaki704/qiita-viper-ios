67cfd3bb0bc022ea232156d9abf77420addaff05
Suzuki Kimiaki <kimiaki@suzukinoMacBook-Pro.local>
2020-05-02 15:33:19 +0900

    🐤 Update AppDelegate.swift
    run swiftformat

--- QiitaVIPER/Application/AppDelegate.swift
+++ QiitaVIPER/Application/AppDelegate.swift
@@ -10,9 +10,8 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
 }
-
