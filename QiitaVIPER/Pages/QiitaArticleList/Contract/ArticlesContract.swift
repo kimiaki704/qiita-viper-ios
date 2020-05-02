a7a0e1752ba73193b487b50efbdcf513fb323857
Suzuki Kimiaki <kimiaki@suzukinoMacBook-Pro.local>
2020-05-02 20:50:36 +0900

    🐤 Update ArticlesContract.swift

--- QiitaVIPER/Pages/QiitaArticleList/Contract/ArticlesContract.swift
+++ QiitaVIPER/Pages/QiitaArticleList/Contract/ArticlesContract.swift
@@ -6,13 +6,36 @@
 //
 
 import Model
+import UIKit
+
+protocol ArticlesView {
+    var presenter: ArticlesPresentation! { get set }
+
+    func showNoContentScreen()
+    func showArticlesData(_ articles: [QiitaArticle])
+}
+
+protocol ArticlesPresentation: class {
+    var view: ArticlesView? { get set }
+    var interactor: ArticlesUseCase! { get set }
+    var router: ArticlesWireframe! { get set }
+
+    func viewDidLoad()
+    func didClickSortButton()
+    func didSelectArticle(_ article: QiitaArticle)
+}
 
 protocol ArticlesUseCase: class {
-    weak var output: ArticlesInteractorOutput! { get set }
+    var output: ArticlesInteractorOutput! { get set }
     func fetchArticles(page: Int, pagePer: Int, limit: Int)
 }
 
 protocol ArticlesInteractorOutput: class {
     func articlesFetched(_ articles: [QiitaArticle])
     func articlesFetchFailed(_ error: Error)
+}
+
+protocol ArticlesWireframe: class {
+    var viewController: UIViewController? { get set }
+    static func assembleModule() -> UIViewController
 }
