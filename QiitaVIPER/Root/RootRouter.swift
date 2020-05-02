545f27ac6282aa3956e855e7c92b1617c556e3fa
Suzuki Kimiaki <kimiaki@suzukinoMacBook-Pro.local>
2020-05-02 16:53:44 +0900

    🍹 Add RootRouter.swift

---
+++ QiitaVIPER/Root/RootRouter.swift
@@ -0,0 +1,17 @@
+//
+//  RootRouter.swift
+//  QiitaVIPER
+//
+//  Created by Suzuki Kimiaki on 2020/05/02.
+//
+
+import UIKit
+
+class RootRouter: RootWireframe {
+    static var appResolver: Resolver { Resolver.shared }
+    
+    func presentArticleListViewController(in window: UIWindow) {
+        window.makeKeyAndVisible()
+        #warning("TODO: RouterからArticleListViewControllerをrootViewにset")
+    }
+}
