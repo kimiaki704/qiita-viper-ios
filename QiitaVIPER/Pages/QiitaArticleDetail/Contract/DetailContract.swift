e3278608ca919a544dcc245e23747bd258718a04
kimiaki704 <kimiaki704@gmail.com>
2020-05-03 19:55:12 +0900

    🍹 Add DetailContract.swift

---
+++ QiitaVIPER/Pages/QiitaArticleDetail/Contract/DetailContract.swift
@@ -0,0 +1,26 @@
+//
+//  DetailContract.swift
+//  QiitaVIPER
+//
+//  Created by 鈴木 公章 on 2020/05/03.
+//
+
+import Model
+import UIKit
+
+protocol DetailView: class {
+    var presenter: DetailPresentation! { get set }
+
+    func showDetailData(_ article: QiitaArticle)
+}
+
+protocol DetailPresentation: class {
+    var view: DetailView? { get set }
+
+    func viewDidLoad()
+}
+
+protocol DetailWireframe: class {
+    static var appResolver: Resolver { get }
+    static func assembleModule() -> UIViewController
+}
