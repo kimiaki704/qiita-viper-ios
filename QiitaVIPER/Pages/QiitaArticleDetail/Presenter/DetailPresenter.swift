f715de5d8b5bb6704dd72fdaae568485d2171c79
kimiaki704 <kimiaki704@gmail.com>
2020-05-03 20:48:02 +0900

    🍹 Add DetailPresenter.swift

---
+++ QiitaVIPER/Pages/QiitaArticleDetail/Presenter/DetailPresenter.swift
@@ -0,0 +1,14 @@
+//
+//  DetailPresenter.swift
+//  QiitaVIPER
+//
+//  Created by 鈴木 公章 on 2020/05/03.
+//
+
+class DetailPresenter: DetailPresentation {
+    var view: DetailView?
+
+    func viewDidLoad() {
+        print(view)
+    }
+}
