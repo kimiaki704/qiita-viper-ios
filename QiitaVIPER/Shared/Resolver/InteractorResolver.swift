4145c58df301ac6c46af048a4ed61380eee73e43
kimiaki704 <kimiaki704@gmail.com>
2020-05-05 08:07:36 +0900

    🍹 Add InteractorResolver.swift

---
+++ QiitaVIPER/Shared/Resolver/InteractorResolver.swift
@@ -0,0 +1,12 @@
+//
+//  InteractorResolver.swift
+//  QiitaVIPER
+//
+//  Created by 鈴木 公章 on 2020/05/05.
+//
+
+extension Resolver {
+    func resolveArticlesInteractor() -> ArticlesInteractor {
+        ArticlesInteractor()
+    }
+}
